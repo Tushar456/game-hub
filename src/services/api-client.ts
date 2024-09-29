722f2aafd9b8211d0f467b4532011aa0eca3c44e
Tushar <tusharchaurasia456@gmail.com>
2024-09-29 22:13:55 +0530

    pushing react 1st version

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default axios.create({
     params: {
-        key: 'f9684ad2cf0b4d528274bf7bc9760a49'
+        key: '<api_key>'
     },
     baseURL: 'https://api.rawg.io/api',
 })
